591880d7e5508400b219bd16b0f144c0dbc46eed
Mouna876 <mounanouira1811@gmail.com>
2024-11-21 16:29:30 +0100

    Ajoutez des fichiers projet.

---
+++ OrderAPI/Consumer/ProductCreatedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Models;
+using OrderAPI.Data;
+using Shared.Models;
+
+namespace OrdersAPI.Consumer
+{
+    public class ProductCreatedConsumer : IConsumer<ProductCreated>
+    {
+        private readonly OrderAPIContext _OrdersAPIContext;
+
+        public ProductCreatedConsumer(OrderAPIContext ordersAPIContext)
+        {
+            _OrdersAPIContext = ordersAPIContext;
+        }
+
+        public async Task Consume(ConsumeContext<ProductCreated> context)
+        {
+            var newProduct = new Product
+            {
+                // Id = context.Message.Id,
+                Name = context.Message.Name
+            };
+
+            _OrdersAPIContext.Add(newProduct);
+            await _OrdersAPIContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ OrderAPI/Data/OrderAPIContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace OrderAPI.Data
+{
+    public class OrderAPIContext : DbContext
+    {
+        public OrderAPIContext (DbContextOptions<OrderAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Orders> Orders { get; set; } = default!;
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
